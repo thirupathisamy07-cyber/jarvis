5ea8d6049c4194bb028d71552f4dc43baad426db
thirupathisamy07-cyber <thirupathisamy07@gmail.com>
2026-03-17 22:37:55 +0530

    Update pipeline {.groovy

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -1,27 +1,23 @@
 pipeline {
- agent any
- stages {
- stage('Checkout Code') {
- steps {
- git 'https://github.com/thirupathisamy07-cyber/jarvis'
- }
-MKCE_CSE Page 26
- 
- }
- stage('Build') {
- steps {
- sh 'javac Main.java' // Example build step for Java
- }
- }
- stage('Test') {
- steps {
- sh 'java Main' // Running the compiled Java program
- }
- }
- stage('Deploy') {
- steps {
- echo 'Deploying application...'
- }
- }
- }
+    agent any
+
+    stages {
+        stage('Clone') {
+            steps {
+                git branch: 'main', url: 'https://github.com/thirupathisamy07-cyber/jarvis.git'
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                bat 'pip install requests pyttsx3 SpeechRecognition psutil pillow'
+            }
+        }
+
+        stage('Run Jarvis') {
+            steps {
+                bat 'python jarvis.py'
+            }
+        }
+    }
 }
