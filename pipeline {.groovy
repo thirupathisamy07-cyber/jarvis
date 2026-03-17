ecc4b4b517feaf8f96455939d8825fec577b32b6
thirupathisamy07-cyber <thirupathisamy07@gmail.com>
2026-03-17 22:19:42 +0530

    Add files via upload

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -1,17 +1,27 @@
 pipeline {
-    agent any
-
-    stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'main', url: 'https://github.com/thirupathisamy07-cyber/jarvis.git'
-            }
-        }
-
-        stage('Test') {
-            steps {
-                bat 'echo Jenkins working with main branch!'
-            }
-        }
-    }
+ agent any
+ stages {
+ stage('Checkout Code') {
+ steps {
+ git 'https://github.com/thirupathisamy07-cyber/jarvis'
+ }
+MKCE_CSE Page 26
+ 
+ }
+ stage('Build') {
+ steps {
+ sh 'javac Main.java' // Example build step for Java
+ }
+ }
+ stage('Test') {
+ steps {
+ sh 'java Main' // Running the compiled Java program
+ }
+ }
+ stage('Deploy') {
+ steps {
+ echo 'Deploying application...'
+ }
+ }
+ }
 }
